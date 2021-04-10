26207f7cbe4363f6461e09ddfb67f33bce5fd161
bzamora020 <bryan.aldair.zamora@gmail.com>
2021-04-10 01:44:21 -0700

    Added css and elements to viewPost page

--- frontend/src/ViewPost/ViewPost.js
+++ frontend/src/ViewPost/ViewPost.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React from "react";
+import './ViewPost.css';
 
 function ViewPost() {
-    return (
-        <div>
-            <h1>This is the view post page!</h1>
-        </div>
-    )
+  return (
+    <div className= "box">
+      <div className= "topBox">
+        <h3 style={{ color: 'white' }}>RandomUser:</h3> <h3> Mr Stark I am not feeling so good</h3>
+      </div>
+      <p className= "blog">Hello there, this is some text in a text area</p>
+      <label>Comment:</label>
+      <br></br>
+      <input type="text" /> <input type="submit" value="Submit" />
+    </div>
+  );
 }
 
 export default ViewPost;
