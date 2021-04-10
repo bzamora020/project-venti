6fa7de95b378fe1da9cc6930678cc04234048fcf
Asarel Castellanos <acaste16@ucsc.edu>
2021-04-10 11:31:00 -0700

    Updated navbar.

--- frontend/src/Navbar/Navbar.js
+++ frontend/src/Navbar/Navbar.js
@@ -3,23 +3,17 @@ import { Link, Route } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import './Navbar.css'
 
-
 function Navbar() {
     return (
         <div className="navbar">
-            <Button variant="contained" styles= {styles.Button}>
-                <Link to="/createpost">I need some love</Link>
+            <Button id="leftbutton" className="button" variant="contained" color="#FFFFFF">
+                <Link className="link" to="/createpost">I need some love</Link>
             </Button>
-            <h1 className="title">Welcome</h1>
-            <Button variant="contained" color="#ECB143">
-                <Link to="/viewpost">Cheer someone up!</Link>
+            <h1 className="title">Venti</h1>
+            <Button id="rightbutton" className="button" variant="contained" color="#FFFFFF">
+                <Link className="link" to="/viewpost">Cheer someone up!</Link>
             </Button>
         </div>
     )
-}
-const styles = {
-    Button:{
-        background: "#ECB143"
-    }
 }
 export default Navbar;
