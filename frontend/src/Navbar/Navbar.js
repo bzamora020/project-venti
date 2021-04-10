1632d47fe890a8c23cbd67d10fe14265879f944b
Asarel Castellanos <acaste16@ucsc.edu>
2021-04-10 14:07:47 -0700

    Navbar.

--- frontend/src/Navbar/Navbar.js
+++ frontend/src/Navbar/Navbar.js
@@ -7,7 +7,7 @@ function Navbar() {
     return (
         <div className="navbar">
             <Button id="leftbutton" className="button" variant="contained" color="#FFFFFF">
-                <Link className="link" to="/createpost">I need some love</Link>
+                <Link className="link" to="/">Home</Link>
             </Button>
             <h1 className="title">Venti</h1>
             <Button id="rightbutton" className="button" variant="contained" color="#FFFFFF">
