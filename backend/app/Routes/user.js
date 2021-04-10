fce00d39092e2e23b03d482824d18170dc86c857
Nabil <duomaster01@gmail.com>
2021-04-10 15:41:17 -0700

    getHome endpoint works now

--- backend/app/Routes/user.js
+++ backend/app/Routes/user.js
@@ -3,6 +3,92 @@ const user = express.Router();
 const session = require('./session');
 const db = require('../db');
 
+function buildPost(post){
+    return new Promise((resolve, reject)=>{
+
+        let postObj = {
+            id: post.id,
+            title: post.title,
+            content: post.content,
+            comments: [],
+        };
+        
+        // Builds comments for post
+        let getPostCommentsQuery = `SELECT id, user_id, content, likes FROM posts_comments WHERE post_id=${post.id}`;
+        db.query(getPostCommentsQuery, (error, comments)=>{
+            if(error){
+                console.error(error);
+                reject(Error("Failed to get user posts' comments"));
+            }
+            else{
+                let counter = 0;
+                for(i = 0; i < comments.length; i++){
+                    let buildComments = new Promise((resolve, reject)=>{
+                        let comment = comments[i];
+                        let getCommenterNameQuery = `SELECT name FROM users WHERE id=${db.escape(comment.user_id)}`;
+                        db.query(getCommenterNameQuery, (error, results)=>{
+                            if(error){
+                                console.error(error);
+                                reject(Error("Unable to get commenter name"));
+                            }
+                            else{
+                                comment.name = results[0].name;
+                                postObj.comments.push(comment);
+                                resolve(postObj);
+                            }
+                        });
+                    });
+                    buildComments.then((postObj)=>{
+
+                        counter++;
+                        if(counter == (comments.length)){
+                            resolve(postObj);
+                        }
+                    })
+                    .catch((error)=>{
+                        reject(Error(error));
+                    })
+                }                
+            }
+        });
+
+
+    })
+}
+
+function buildHomeObjPosts(posts){
+    return new Promise((resolve, reject)=>{
+        let homePosts = [];
+
+        let counter = 0;
+        for(i = 0; i < posts.length; i++){
+            let buildPostPromise = new Promise((resolve, reject)=>{
+                let post = posts[i];
+                buildPost(post)
+                    .then((postObj)=>{
+                        homePosts.push(postObj);
+                        resolve(homePosts);
+                    })
+                    .catch((error)=>{
+                        reject(Error(error));
+                    })
+            })
+
+            buildPostPromise.then((homePosts)=>{
+                counter++;
+                if(counter == posts.length){
+                    console.log(homePosts);
+                    resolve(homePosts);
+                }
+            })
+            .catch((error)=>{
+                console.error(error);
+                reject(Error(error));
+            })
+        }
+    })
+}
+
 user.post('/getHome', (req, res)=>{
     let sessionToken = req.cookies['session'];
     session.verifySessionToken(sessionToken)
@@ -19,55 +105,27 @@ user.post('/getHome', (req, res)=>{
                         posts: [],
                     }
                     let getUserPostsQuery = `SELECT id, title, content FROM posts WHERE user_id=${user.user_id}`;
-                    db.query(getUserPostsQuery, (error, results)=>{
+                    db.query(getUserPostsQuery, (error, posts)=>{
                         if(error){
                             console.error(error);
                             res.status(500).send({error: "Failed to get user posts"});
                         }
                         else{
-                            if(results.length > 0){
-                                for(post in results){
-                                    let postObj = {
-                                        id: post.id,
-                                        title: post.title,
-                                        content: post.conent,
-                                        comments: [],
-                                    }
-                                    let getPostCommentsQuery = `SELECT id, user_id, content, likes FROM posts_comments WHERE post_id=${post.id}`;
-                                    db.query(getPostCommentsQuery, (error, results)=>{
-                                        if(error){
-                                            console.error(error);
-                                            res.status(500).send({error: "Failed to get user posts' comments"});
-                                        }
-                                        else{
-                                            for(comment in results){
-                                                let getCommenterNameQuery = `SELECT name FROM users WHERE id=${db.escape(comment.user_id)}`;
-                                                db.query(getCommenterNameQuery, (error, results)=>{
-                                                    if(error){
-                                                        console.error(error);
-                                                        res.status(500).send({error: "Unable to get commenter name"});
-                                                    }
-                                                    else{
-                                                        comment.name = results[0].name;
-                                                        postObj.comments.push(comment);
-                                                    }
-                                                });
-                                            }
-                                        }
-                        
+                            if(posts.length > 0){
+                                buildHomeObjPosts(posts)
+                                    .then((homeObjPosts)=>{
+                                        homeObj.posts = homeObjPosts;
+                                        res.status(200).send(homeObj);
                                     })
-                                    .then(()=>{
-                                        homeObj.posts.push(postObj);
+                                    .catch((error)=>{
+                                        console.error(error);
+                                        res.status(500).send({error:"Something went wrong D:"});
                                     })
-                                }
                             }
                             else{
                                 res.status(200).send({msg: "Post something to make this place feel like home"});
                             }
                         }
-                    })
-                    .then(()=>{
-                        res.status(200).send(homeObj);
                     })
                 }
             });
