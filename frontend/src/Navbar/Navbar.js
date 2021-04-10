c4c741322f099177c9b9daa981cf70b130280099
bzamora020 <bryan.aldair.zamora@gmail.com>
2021-04-10 02:14:18 -0700

    I forgot what I changed

--- frontend/src/Navbar/Navbar.js
+++ frontend/src/Navbar/Navbar.js
@@ -7,15 +7,19 @@ import './Navbar.css'
 function Navbar() {
     return (
         <div className="navbar">
-            <Button variant="contained" color="primary">
+            <Button variant="contained" styles= {styles.Button}>
                 <Link to="/createpost">I need some love</Link>
             </Button>
             <h1 className="title">Welcome</h1>
-            <Button variant="contained" color="primary">
+            <Button variant="contained" color="#ECB143">
                 <Link to="/viewpost">Cheer someone up!</Link>
             </Button>
         </div>
     )
 }
-
+const styles = {
+    Button:{
+        background: "#ECB143"
+    }
+}
 export default Navbar;
--- frontend/src/ViewPost/ViewPost.js
+++ frontend/src/ViewPost/ViewPost.js
@@ -5,7 +5,7 @@ function ViewPost() {
   return (
     <div className= "box">
       <div className= "topBox">
-        <h3 style={{ color: 'white' }}>RandomUser:</h3> <h3> Mr Stark I am not feeling so good</h3>
+        <h2 style={{ color: 'white' }}>RandomUser:</h2> <h2> Mr Stark I am not feeling so good</h2>
       </div>
       <p className= "blog">Hello there, this is some text in a text area</p>
       <label>Comment:</label>
