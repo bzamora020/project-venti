f3b632a8d78c58e45c38ea7df90d02b7b2cf9ecd
Nabil <duomaster01@gmail.com>
2021-04-10 16:40:48 -0700

    able to server npm run build files

--- backend/app/app.js
+++ backend/app/app.js
@@ -1,26 +1,29 @@
 const express = require('express');
 const app = express();
-const port = 8000;
+const port = 8080;
 const userRouter = require('./Routes/user');
 const authRouter = require('./Routes/auth');
 const postRouter = require('./Routes/post');
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
+const path = require('path');
 const cookieParser = require('cookie-parser');
 
 app.use(cookieParser());
-app.use(helmet());
 app.use(bodyParser.json())
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+
+
+const root = require('path').join(__dirname,'build')
+
+app.use(express.static(root));
+app.get("*", (req, res) => {
+    res.sendFile('index.html', { root });
 });
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/posts', postRouter);
-
-app.use(express.static(__dirname + '/public'));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
