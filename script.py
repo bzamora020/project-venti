5430d23cb0c064d1e79dc031aa8ccfdf5bd726e4
elmerv <vasquezelmer101@gmail.com>
2021-04-10 12:51:16 -0700

    updating similarities before testing

--- script.py
+++ script.py
@@ -1,24 +1,26 @@
 from fuzzywuzzy import fuzz
 from fuzzywuzzy import process
-
+import json
 def takeRatio(elem):
-    return elem['avg']
-userPosts = []
-generalPosts = []
+    return elem["avg"]
+
+
+with open('data.json') as json_file:
+    posts = json.load(json_file)
+
 ratiosOfGenPosts = {}
 ratioNumber = []
 orderedPosts = []
 
-for x in generalPosts: 
-    for y in userPosts: 
-        ratio = fuzz.ratio(x,y); 
-        ratiosOfGenPosts.setdefault(x,[])
-        ratiosOfGenPosts[x].append(ratio)
- 
-for x in generalPosts:
-    avg = sum(ratiosOfGenPosts[x])/len(ratiosOfGenPosts[x])
-    ratioNumber.append({"id":x.id,"text":x.test,"avg":avg})
+for x in posts["userPosts"]: 
+    for y in posts["generalPosts"]: 
+        ratio = fuzz.ratio(x,y)
+        ratiosOfGenPosts.setdefault(x[id],[])
+        ratiosOfGenPosts[x["id"]].append(ratio)
 
-ratioNumber.sort(key=takeRatio)
+for x in posts['generalPosts']:
+    avg = sum(ratiosOfGenPosts[x['id']])/len(ratiosOfGenPosts[x['id']])
+    ratioNumber.append({"id":x["id"],"text":x["text"],"avg":avg})
 
-    
+ratioNumber.sort(reverse = True,key=takeRatio)
+print(ratioNumber)
