96029d6e41598e0e5a28c195a211f696ea2ad0d1
bzamora020 <bryan.aldair.zamora@gmail.com>
2021-04-11 04:38:20 -0700

    popup almost done

--- frontend/src/Home/Popup.js
+++ frontend/src/Home/Popup.js
@@ -4,10 +4,8 @@ import './Popup.css';
 function Popup(props) {
   return (props.trigger) ? (
     <div className="popup">
-      <div className="popup-inner">
-        <button className="close-btn" onClick={() => props.setTrigger(false)}> Close</button>
+        <button className="close-btn" onClick={() => props.setTrigger(false)}> X</button>
         {props.children}
-      </div>
     </div>
   ) : "";
 }
--- frontend/src/ViewUserPosts/UserPost.js
+++ frontend/src/ViewUserPosts/UserPost.js
@@ -1,29 +1,33 @@
 import React from "react";
 
 import { Button } from "@material-ui/core";
-import littleIcon from "./littleIcon.png"
+import littleIcon from "./littleIcon.png";
 import Popup from "../Home/Popup";
-import { useState } from 'react';
+import { useState } from "react";
 
 import "./UserPost.css";
 
-
-function UserPost (props) {
+function UserPost(props) {
   const [buttonPopup, setButtonPopup] = useState(false);
   return (
-  <div>
-    <Button onClick= {() => setButtonPopup(true)} id = "messageCard">
-      <div id="testing">
-        <img id = "liIcon" src = {littleIcon} />
-        <p id = "postTittle">{props.title}</p>
-        <p id="message">{props.content}</p>
-      </div>
-    </Button>
-    <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
-      <h3> Testing popup</h3>
-    </Popup>
-  </div>
-  )
+    <div>
+      <Button onClick={() => setButtonPopup(true)} id="messageCard">
+        <div id="testing">
+          <img id="liIcon" src={littleIcon} />
+          <p id="postTittle">{props.title}</p>
+          <p id="message">{props.content}</p>
+        </div>
+      </Button>
+      <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
+        <div id="shadows">
+          <div id="popups">
+            <p id="popTittle">{props.title}</p>
+            <p id="popMessage">{props.content}</p>
+          </div>
+        </div>
+      </Popup>
+    </div>
+  );
 }
 
 export default UserPost;
