4bf707571f6fd7ea2f00fa5c3a2f18a6a5eee005
Asarel Castellanos <asarelcastellanos@Asarels-MacBook-Pro.local>
2021-04-10 23:37:21 -0700

    Dyamically shows posts of the user from a database.

--- frontend/src/ViewUserPosts/UserPost.js
+++ frontend/src/ViewUserPosts/UserPost.js
@@ -1,48 +1,10 @@
 import React from 'react';
 
-
-class UserPost extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state ={
-            homeData: null
-        }
-    }
-
-    componentDidMount(){
-        fetch('/api/user/getHome', {
-            method: 'POST',
-            headers:{
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(this.state),
-          })
-          .then((resp)=>{
-            return resp.json();
-          })
-          .then((data)=>{
-            console.log(data);
-            this.setState({
-                homeData: data
-            })
-          })
-          .catch((error)=>{
-            console.error(error);
-          })
-    }
-
-    render(){
-        return(
-            <div>
-                <h1></h1>
-                <p>{this.props.header}</p>
-                <p>{this.props.comment}</p>
-            </div>
-        )
-    }
-
-}
-
+const UserPost = (props) => (
+  <div>
+    <p>{props.title}</p>
+    <p>{props.content}</p>
+  </div>
+)
 
 export default UserPost;
--- frontend/src/ViewUserPosts/UsersPosts.js
+++ frontend/src/ViewUserPosts/UsersPosts.js
@@ -1,19 +1,47 @@
 import React from 'react';
 import UserPost from './UserPost';
 
-const UserPosts = (props) => {
-    const allPosts = [
-        { header: "Header1", comment: "Comment1"},
-        { header: "Header2", comment: "Comment2"},
-        { header: "Header3", comment: "Comment3"}
-    ];
-    return (
-        <section>
-            {allPosts.map((data) => (
-                <UserPost header={data.header} comment={data.comment}/>
-            ))}
-        </section>
-    )
+class UserPosts extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            homeData: null
+        }
+    }
+    componentDidMount() {
+        fetch('/api/user/getHome', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(this.state),
+        })
+            .then((resp) => {
+                return resp.json();
+            })
+            .then((data) => {
+                console.log(data);
+                this.setState({
+                    homeData: data
+                })
+            })
+            .catch((error) => {
+                console.error(error);
+            })
+    }
+    render() {
+        return (
+            <section>
+                {
+                    this.state.homeData &&
+                    this.state.homeData.posts.map((data) => (
+                        <UserPost title={data.title} content={data.content} />
+                    ))
+                }
+            </section >
+        )
+    }
 }
 
 export default UserPosts;
