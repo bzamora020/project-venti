12bd7b0cd6fb44ecd49fbe7fce2bda43b1303f12
Asarel Castellanos <asarelcastellanos@Asarels-MacBook-Pro.local>
2021-04-11 04:37:25 -0700

    Added navbar changes to view posts.

--- frontend/src/ViewPost/ViewPost.js
+++ frontend/src/ViewPost/ViewPost.js
@@ -4,12 +4,11 @@ import heart2 from "./heart2.png";
 import upArrow from "./arrowUp.png";
 import downArrow from "./arrowDown.png";
 
-import Navbar from "../Navbar/Navbar";
+import { Link, Route } from 'react-router-dom';
 import { Button } from "@material-ui/core";
-let title = "Be Kind To Others!";
 
-class ViewPost extends React.Component{
-  constructor(props){
+class ViewPost extends React.Component {
+  constructor(props) {
     super(props);
     this.state = {
       feed: [],
@@ -18,61 +17,69 @@ class ViewPost extends React.Component{
     }
   }
 
-  componentDidMount(){
+  componentDidMount() {
     console.log("HERE");
     fetch('/api/user/feed', {
       method: 'POST',
-      headers:{
+      headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({}),
     })
-    .then((resp)=>{
-      return resp.json();
-    })
-    .then((data)=>{
-      console.log(data);
-      this.setState({feed:data})
-      this.getPost(this.state.feed_index);
-    })
-    .catch((error)=>{
-      console.error(error);
-    })
+      .then((resp) => {
+        return resp.json();
+      })
+      .then((data) => {
+        console.log(data);
+        this.setState({ feed: data })
+        this.getPost(this.state.feed_index);
+      })
+      .catch((error) => {
+        console.error(error);
+      })
   }
 
-  getPost(feed_index){
+  getPost(feed_index) {
     fetch('/api/posts/getPost', {
       method: 'POST',
-      headers:{
+      headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify(this.state.feed[this.state.feed_index]),
     })
-    .then((resp)=>{
-      return resp.json();
-    })
-    .then((data)=>{
-      console.log(data);
-      this.setState({post: data})
-    })
-    .catch((error)=>{
-      console.error(error);
-    })
+      .then((resp) => {
+        return resp.json();
+      })
+      .then((data) => {
+        console.log(data);
+        this.setState({ post: data })
+      })
+      .catch((error) => {
+        console.error(error);
+      })
   }
 
-  render(){
+  render() {
     return (
       <div>
-        <Navbar title={title} />
+        <div className="navbar">
+          <Link className="link" to="/home">
+            <Button id="leftbutton" className="button" variant="contained" color="#FFFFFF">
+              Home
+              </Button>
+          </Link>
+          <h1 className="title">Be Kind to Others!</h1>
+        </div>
         {
           this.state.feed.length > 0 &&
           <div>
-            <Button id = "upArr" onclick={()=>{
-                if(this.state.feed_index > 0){
-                  this.setState({feed_index: this.state.feed_index - 1})}
-                  this.getPost(this.state.feed_index - 1);
-                }
-              }>
+            <Button id="upArr" onclick={() => {
+              if (this.state.feed_index > 0) {
+                this.setState({ feed_index: this.state.feed_index - 1 })
+              }
+              this.getPost(this.state.feed_index - 1);
+            }
+            }>
               <img src={upArrow} />
             </Button>
             <div className="box">
@@ -92,12 +99,13 @@ class ViewPost extends React.Component{
                 </Button>
               </div>
             </div>
-            <Button id = "doArr" onclick={()=>{
-                if(this.state.feed_index < this.state.feed.length - 1){
-                  this.setState({feed_index: this.state.feed_index + 1})}
-                  this.getPost(this.state.feed_index + 1);
-                }
-              }>
+            <Button id="doArr" onclick={() => {
+              if (this.state.feed_index < this.state.feed.length - 1) {
+                this.setState({ feed_index: this.state.feed_index + 1 })
+              }
+              this.getPost(this.state.feed_index + 1);
+            }
+            }>
               <img src={downArrow} />
             </Button>
           </div>
