86a242f70627dac6bec9ddd191574ce5a14b0338
bzamora020 <bryan.aldair.zamora@gmail.com>
2021-04-11 03:06:45 -0700

    popup shows up and closes, needs style

--- frontend/src/ViewUserPosts/UserPost.js
+++ frontend/src/ViewUserPosts/UserPost.js
@@ -2,20 +2,28 @@ import React from "react";
 
 import { Button } from "@material-ui/core";
 import littleIcon from "./littleIcon.png"
+import Popup from "../Home/Popup";
+import { useState } from 'react';
 
 import "./UserPost.css";
 
 
-const UserPost = (props) => (
+function UserPost (props) {
+  const [buttonPopup, setButtonPopup] = useState(false);
+  return (
   <div>
-    <Button id = "messageCard">
+    <Button onClick= {() => setButtonPopup(true)} id = "messageCard">
       <div id="testing">
         <img id = "liIcon" src = {littleIcon} />
         <p id = "postTittle">{props.title}</p>
         <p id="message">{props.content}</p>
       </div>
     </Button>
+    <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
+      <h3> Testing popup</h3>
+    </Popup>
   </div>
-)
+  )
+}
 
 export default UserPost;
