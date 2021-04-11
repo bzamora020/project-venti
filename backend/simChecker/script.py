f2657582553b7db6faab33c521baabe0a3a81e65
elmerv <elfvasqu@ucsc.edu>
2021-04-11 00:08:24 -0700

    pushing sim-checker again

---
+++ backend/simChecker/script.py
@@ -0,0 +1,29 @@
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
+import json
+import sys
+
+def takeRatio(elem):
+    return elem["avg"]
+
+# with open('data.json') as json_file:
+#     posts = json.load(json_file)
+
+posts = json.load(sys.argv[0])
+ratiosOfGenPosts = {}
+ratioNumber = []
+orderedPosts = []
+
+for x in posts["userPosts"]: 
+    for y in posts["generalPosts"]: 
+        ratio = fuzz.ratio(x,y)
+        ratiosOfGenPosts.setdefault(x[id],[])
+        ratiosOfGenPosts[x["id"]].append(ratio)
+
+for x in posts['generalPosts']:
+    avg = sum(ratiosOfGenPosts[x['id']])/len(ratiosOfGenPosts[x['id']])
+    ratioNumber.append({"id":x["id"],"text":x["text"],"avg":avg})
+
+ratioNumber.sort(reverse = True,key=takeRatio)
+print(ratioNumber)
+sys.stdout.flush()
